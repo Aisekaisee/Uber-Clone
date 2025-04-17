ad14d12920d421808df31c50cd76c2d4c91e685a
Aisekaisee <kohlivipul48@gmail.com>
2025-04-18 01:26:33 +0530

    Navigation done

--- App.js
+++ App.js
@@ -2,13 +2,49 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import EatScreen from './screens/EatScreen';
 import { Provider } from 'react-redux';
 import store from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              headerShown:false
+            }}
+            />
+            <Stack.Screen 
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown:false
+            }}
+            />
+            <Stack.Screen 
+            name="EatScreen"
+            component={EatScreen}
+            options={{
+              headerShown:false
+            }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,35 +1,49 @@
 import { StyleSheet, Text, TouchableOpacity, View,FlatList,Image } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/AntDesign';
+import { useNavigation } from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen';
 
 const data=[
     {
         id:"123",
         title:"Get a ride",
         image:"https://links.papareact.com/3pn",
-        screen:"Map Screen"     
+        screen:"MapScreen"     
     },
     {
         id:"456",
         title:"Order food",
         image:"https://links.papareact.com/28w",
-        screen:"Eat Screen"     
+        screen:"EatScreen"     
     }
 ]
 
 const NavOptions = () => {
-  return (
+
+    const navigation = useNavigation();
+    return (
     <FlatList
     data={data}
     keyExtractor={(item) => (item.id) }
     horizontal
     renderItem={({item}) => (
-        <TouchableOpacity> 
+        <TouchableOpacity 
+        onPress={() => navigation.navigate(item.screen)}
+        className="p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40"> 
             <View>
                 <Image
                 style={{width:120, height:120, resizeMode:'contain'}}
                 source={{uri:item.image}}
                 />
-                <Text>{item.title}</Text>
+                <Text className="mt-2 text-lg font-semibold">{item.title}</Text>
+                <Icon 
+                className="p-3 bg-black rounded-full w-10 mt-4"
+                name="arrowright" 
+                color="white" 
+                type="antdesign"
+                size={15}
+                />
             </View>
         </TouchableOpacity>
     )}
